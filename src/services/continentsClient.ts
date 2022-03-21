a70abdd37ebe531246fc9a8bf1ba171171e6610a
gllherme <guicampos159@gmail.com>
2022-03-21 20:16:55 -0300

    update export

--- src/services/continentsClient.ts
+++ src/services/continentsClient.ts
@@ -1,8 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const continentsClient = new ApolloClient({
+export const continentsClient = new ApolloClient({
     uri: "https://countries.trevorblades.com",
     cache: new InMemoryCache()
 });
-
-export default continentsClient;
